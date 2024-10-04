500d50db3fe6eb5b65f44082d7b06e03f5b39451
Xuma <humounhamdamov90@gmail.com>
2024-10-04 19:00:54 +0500

    Add files via upload

--- js.js
+++ js.js
@@ -8,11 +8,28 @@ burger.addEventListener('click', () =>{
 
 const swiperAlbum1 = new Swiper('.swiper-album-1', {
     loop: true,
-    slidesPerView: 1,
+    slidesPerView: 0.3,
+    spaceBetween: 8,
+
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
     },
+
+    breakpoints: {
+        500: {
+            slidesPerView: 0.4,
+        },
+
+        700: {
+            slidesPerView: 0.5,
+        },
+
+        900: {
+            slidesPerView: 0.6,
+        }
+    },
+
     allowTouchMove: false,
 
     speed: 20000
@@ -21,13 +38,33 @@ const swiperAlbum1 = new Swiper('.swiper-album-1', {
 
 const swiperAlbum2 = new Swiper('.swiper-album-2', {
     loop: true,
-    slidesPerView: 1,
+    slidesPerView: 0.3,
+    spaceBetween: 8,
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
         reverseDirection: true
     },
+
+    
+    breakpoints: {
+        500: {
+            slidesPerView: 0.4,
+        },
+
+        700: {
+            slidesPerView: 0.5,
+        },
+
+        900: {
+            slidesPerView: 0.6,
+        }
+    },
+
+    
     allowTouchMove: false,
 
     speed: 20000
 });
+
+
